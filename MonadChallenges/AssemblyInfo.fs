d29c37bcb474c72ccff1d59660d73cca8ae8b675
Scott Fleischman <scott.fleischman@logos.com>
2016-04-15 10:56:10 -0700

    Add project stub

---
+++ MonadChallenges/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿namespace MonadChallenges.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("MonadChallenges")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("MonadChallenges")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("f1bb86a0-dd6a-41cc-8fa8-bf578b0c000c")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ MonadChallenges/Program.fs
@@ -0,0 +1,4 @@
+﻿[<EntryPoint>]
+let main argv = 
+    printfn "Challenge accepted"
+    0
